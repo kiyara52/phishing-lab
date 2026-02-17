89c934edfbe6d8ef6b029a9bef6dd21e812e6098
kiyara52 <khyaatisolanki552@gmail.com>
2026-02-17 10:08:46 +0000

    fixed render port

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def analyze():
 		return render_template("result.html",data=data)
 
 if __name__=="__main__":
-	app.run()
+	port = int(os.environ.get("PORT",10000))
+	app.run(host="0.0.0.0",port=port)
